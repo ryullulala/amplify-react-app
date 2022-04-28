eccaf41f5f93f9c399f94b6f8b6c9797092d2811
ByeongRyul Cho <ryullulala@icloud.com>
2022-04-28 21:39:29 +0900

    cryptoapp release

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "cryptoapp": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "cryptoapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    },
+    "auth": {
+        "cryptoappbb65cb3b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import { API } from "aws-amplify";
+import "./App.css";
 function App() {
+  const [input, updateInput] = useState({ limit: 5, start: 0 });
+  const [coins, updateCoins] = useState([]);
+
+  function updateInputValues(type, value) {
+    updateInput({ ...input, [type]: value });
+  }
+
+  async function fetchCoins() {
+    const { limit, start } = input;
+    const data = await API.get(
+      "cryptoapi",
+      "/coins?limit=${limit}&start=${start}"
+    );
+    updateCoins(data.coins);
+  }
+
+  // useEffect(() => {
+  //   fetchCoins();
+  // }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {coins.map((coin, index) => (
+        <div key={index}>
+          <h2>
+            {coin.name} - {coin.symbol}
+          </h2>
+          <h5>${coin.price_usd}</h5>
+        </div>
+      ))}
+      <input
+        onChange={(e) => updateInputValues("limit", e.target.value)}
+        placeholder="limit"
+      />
+      <input
+        onChange={(e) => updateInputValues("start", e.target.value)}
+        placeholder="start"
+      />
+      <button onClick={fetchCoins}>Fetch Coins</button>
     </div>
   );
 }
